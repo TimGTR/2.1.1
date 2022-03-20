f663a9a0d73a328b8c9f3e1fc7293a30e174e16b
Timur Garipov <tr.garipov@yandex.ru>
2022-03-21 00:10:51 +0300

    +tests

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -78,4 +78,49 @@ class MainKtTest {
 
         assertEquals("Комиссия 20 копеек", result)
     }
+
+    @Test
+    fun calcDiscount_Mastercard_0_7500000() {
+        val cardT = "Mastercard"
+        val transactionHistoryT = 0
+        val transactionSumT = 7500000
+
+        val result = calcDiscount(
+            card = cardT,
+            transactionHistory = transactionHistoryT,
+            transactionSum = transactionSumT
+        )
+
+        assertEquals("Комиссия 470 копеек", result)
+    }
+
+    @Test
+    fun calcDiscount_Visa_0_0() {
+        val cardT = "Visa"
+        val transactionHistoryT = 0
+        val transactionSumT = 0
+
+        val result = calcDiscount(
+            card = cardT,
+            transactionHistory = transactionHistoryT,
+            transactionSum = transactionSumT
+        )
+
+        assertEquals("Комиссия 3500 копеек", result)
+    }
+
+    @Test
+    fun calcDiscount_Visa_0_500000() {
+        val cardT = "Visa"
+        val transactionHistoryT = 0
+        val transactionSumT = 500000
+
+        val result = calcDiscount(
+            card = cardT,
+            transactionHistory = transactionHistoryT,
+            transactionSum = transactionSumT
+        )
+
+        assertEquals("Комиссия 0 копеек", result)
+    }
 }
